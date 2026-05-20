d296eefcf2de551e2511bd578741fcf39c36efa5
Iffat <iffatjaved1850@gmail.com>
2026-05-20 13:41:31 +0500

    Add DBHelper

--- Data/DBHelper.cs
+++ Data/DBHelper.cs
@@ -3,7 +3,7 @@ using System.Data;
 using System.Data.SQLite;
 using System.IO;
 using System.Windows.Forms;
-
+//Database helper class for Managing connections
 /// NuGet packages needed:
 /// System.Data.SQLite.Core
 /// BCrypt.Net-Next
